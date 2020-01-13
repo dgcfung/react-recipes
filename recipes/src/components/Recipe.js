e88efcf71a1ecfb4b44d95f7785a9f20ccdc7497
B Legit <fungd@Donalds-MacBook-Air.local>
2020-01-13 18:45:12 -0500

    component 2 progress

--- recipes/src/components/Recipe.js
+++ recipes/src/components/Recipe.js
@@ -4,21 +4,44 @@ const Recipe=(props)=>
 {
     console.log(props)
     const {recipe}= props
+    const {title, activePrep, totalPrep, servings}= recipe
+    const {name, source}=recipe.byline
+
+    // const{activePrep, totalPrep, servings, title}=recipe
+
 // deconstructing to pull recipe out of props, 
 
     return(
-        <header>
-             <h1>{recipe.title}</h1>
-            <cite className="contributors">
-                   <div>By John Doe</div>
-                   <div><a href="https://cooking.nytimes.com/recipes/5593-chicken-and-apricot-masala" target="_blank">Source</a></div>
-                   </cite>
-             <ul id="recipe_meta_data" class="list-group">
-               <li className="list-group-item">Active: 1 Hour</li>
-               <li className="list-group-item">Total: 5 Hours</li>
-               <li className="list-group-item">6 Servings</li>
-             </ul>
-           </header>
+    <div>
+            <h1>{title}</h1>
+        <cite className="contributors">
+                <div>By {name}</div>
+                <div><a href={source} target="_blank">Source</a></div>
+                </cite>
+            <ul id="recipe_meta_data" className="list-group">
+            <li className="list-group-item">Active: {activePrep}</li>
+            <li className="list-group-item">Total: {totalPrep}</li>
+            <li className="list-group-item">{servings} Servings</li>
+            </ul>
+
+        <img id="food_image" src="./media/tikka.jpg" alt="" className="img-thumbnail"/>
+
+        <div id="Ingredients_cont">
+        <h1>Ingredients</h1>
+        <ul>
+            <li> 6 garlic cloves, finely grated</li>
+            <li>4 teaspoons finely grated peeled ginger</li>
+            <li>4 teaspoons ground turmeric</li>
+            <li>2 teaspoons garam masala</li>
+            <li>2 teaspoons ground cumin</li>
+            <li>1 tablespoon kosher salt</li>
+            <li>2 pounds skinless, boneless chicken breasts</li>
+            <li>3 tablespoons ghee (clarified butter) or vegetable oil</li>
+            <li>1 small onion, thinly sliced...</li>
+        </ul>
+    </div>
+</div>
+
 
 //  <article className="article">
 //      <img id="food_image" src="./media/tikka.jpg" alt="" className="img-thumbnail">
