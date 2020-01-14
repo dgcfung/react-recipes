a82c378af9901aa221602b326a39bf803170c4f9
B Legit <fungd@Donalds-MacBook-Air.local>
2020-01-13 19:30:02 -0500

    final components

--- recipes/src/components/Recipe.js
+++ recipes/src/components/Recipe.js
@@ -4,8 +4,9 @@ const Recipe=(props)=>
 {
     console.log(props)
     const {recipe}= props
-    const {title, activePrep, totalPrep, servings}= recipe
+    const {title, ingredients, activePrep, totalPrep, servings, img, preparation}= recipe
     const {name, source}=recipe.byline
+  
 
     // const{activePrep, totalPrep, servings, title}=recipe
 
@@ -24,22 +25,21 @@ const Recipe=(props)=>
             <li className="list-group-item">{servings} Servings</li>
             </ul>
 
-        <img id="food_image" src="./media/tikka.jpg" alt="" className="img-thumbnail"/>
+        <img id="food_image" src= {img} alt="" className="img-thumbnail"/>
 
         <div id="Ingredients_cont">
         <h1>Ingredients</h1>
         <ul>
-            <li> 6 garlic cloves, finely grated</li>
-            <li>4 teaspoons finely grated peeled ginger</li>
-            <li>4 teaspoons ground turmeric</li>
-            <li>2 teaspoons garam masala</li>
-            <li>2 teaspoons ground cumin</li>
-            <li>1 tablespoon kosher salt</li>
-            <li>2 pounds skinless, boneless chicken breasts</li>
-            <li>3 tablespoons ghee (clarified butter) or vegetable oil</li>
-            <li>1 small onion, thinly sliced...</li>
+            {
+            ingredients.map(ingredient => {
+                return <li> {ingredient}</li>
+            })
+        }
         </ul>
     </div>
+    <h1>Preparation</h1>
+    <p>{preparation}</p>
+
 </div>
 
 
